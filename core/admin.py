5f5f21bbf7f0af1bf14c1ecd004de79a8e66eaa9
Yves <byives21@gmail.com>
2021-02-22 04:45:30 -0500

    model created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title','description','priority','createDate','modifiedDate']
+    list_filter = ['priority','createDate']
+    list_per_page = 10
+
+
+admin.site.register(Todo,TodoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Todo(models.Model):
+    PRIORITIES = (
+        ('LOW','LOW'),
+        ('MEDIUM','MEDIUM'),
+        ('HIGH','HIGH'),
+
+    )
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    priority = models.CharField(max_length=8, choices=PRIORITIES, default='MEDIUM')
+    createDate = models.DateTimeField(auto_now_add=True)
+    modifiedDate = models.DateTimeField(null=True)
+    
+    class Meta:
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
+
+    def __str__(self):
+        return self.title
